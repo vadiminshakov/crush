0f65affa5e0998771d876490c7a56a29bee7fbc6
Vadim Inshakov <vadiminshakov@gmail.com>
2026-05-20 21:22:32 +0500

    fix: check status exists

--- internal/ui/model/ui.go
+++ internal/ui/model/ui.go
@@ -4078,7 +4078,9 @@ func (m *UI) toggleInputMode() tea.Cmd {
 
 		m.mode = targetMode
 		m.setEditorPrompt(m.com.Workspace.PermissionSkipRequests())
-		m.status.SetMode(m.mode == uiInputModePlan)
+		if m.status != nil {
+			m.status.SetMode(m.mode == uiInputModePlan)
+		}
 		if err := m.com.Workspace.UpdateAgentModel(context.Background()); err != nil {
 			return util.ReportError(err)()
 		}
